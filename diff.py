545d1c124b8ab4b80473c713fd768f2496a699bd
Chris Duryee <cduryee@redhat.com>
2020-06-09 11:10:57 -0400

    add unified diff view

--- diff.py
+++ diff.py
@@ -3,6 +3,8 @@ from http import HTTPStatus
 import json
 import uuid
 import requests
+import sys
+from difflib import unified_diff
 
 from dictdiffer import diff
 from tqdm.auto import tqdm
@@ -11,6 +13,15 @@ get_host_by_name_url = "https://%s/api/inventory/v1/hosts?display_name=%s"
 get_host_url = "https://%s/api/inventory/v1/hosts/%s"
 get_profile_url = "https://%s/api/historical-system-profiles/v1/profiles/%s"
 get_profile_list_url = "https://%s/api/historical-system-profiles/v1/systems/%s"
+
+
+class SetEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, set):
+            return sorted(list(obj))
+        if isinstance(obj, list):
+            return sorted(obj)
+        return json.JSONEncoder.default(self, obj)
 
 
 def _make_request(url, username, password, ssl_verify):
@@ -34,6 +45,20 @@ def _fetch_comparison(hsps):
         n = n + 1
 
 
+def _fetch_unified_comparison(hsps):
+    n = 0
+    while n < len(hsps) - 1:
+        old = json.dumps(hsps[n], cls=SetEncoder, sort_keys=True, indent=4)
+        new = json.dumps(hsps[n + 1], cls=SetEncoder, sort_keys=True, indent=4)
+        yield unified_diff(
+            old.split("\n"),
+            new.split("\n"),
+            fromfile=hsps[n]["captured_date"],
+            tofile=hsps[n + 1]["captured_date"],
+        )
+        n = n + 1
+
+
 def get_hsp(profile_id, args):
     result = _make_request(
         get_profile_url % (args.api_hostname, profile_id),
@@ -50,6 +75,30 @@ def _is_uuid(input_string):
         return True
     except:
         return False
+
+        {
+            "base_url": "https://repo.skype.com/rpm/stable/",
+            "enabled": true,
+            "gpgcheck": true,
+            "id": "skype-stable",
+            "name": "skype (stable)",
+        },
+
+
+def _parse_yum_repos(repos):
+    parsed_repos = set()
+    repo_template = "[%s] %s enabled: %s gpgcheck: %s"
+    for repo in repos:
+        parsed_repos.add(
+            repo_template
+            % (
+                repo.get("name"),
+                repo.get("base_url"),
+                repo.get("enabled"),
+                repo.get("gpgcheck"),
+            )
+        )
+    return parsed_repos
 
 
 def _parse_network_interfaces(interfaces):
@@ -100,6 +149,7 @@ def clean_hsp(hsp):
     hsp["enabled_services"] = {s for s in hsp["enabled_services"]}
     hsp["dnf_modules"] = _parse_dnf_modules(hsp["dnf_modules"])
     hsp["network_interfaces"] = _parse_network_interfaces(hsp["network_interfaces"])
+    hsp["yum_repos"] = _parse_yum_repos(hsp["yum_repos"])
     del hsp["id"]
     del hsp["last_boot_time"]  # this toggles and is not usable currently
     return hsp
@@ -122,6 +172,14 @@ parser.add_argument(
     help="disable SSL hostname verification (only useful for testing)",
 )
 parser.set_defaults(tls_validation=True)
+
+parser.add_argument(
+    "--diff-view",
+    dest="diff_view",
+    action="store_true",
+    help="show data in diff view instead of as report",
+)
+parser.set_defaults(diff_view=False)
 
 args = parser.parse_args()
 
@@ -166,6 +224,13 @@ for profile in tqdm(profiles, unit="profile"):
 
 sorted_hsps = sorted(hsps, key=lambda hsp: hsp.get("captured_date"))
 
+if args.diff_view:
+    for unified in _fetch_unified_comparison(sorted_hsps):
+        for comparison in unified:
+            print(comparison)
+    sys.exit(0)
+
+# TODO:  refactor and put "diff_view" check in an if/else; get rid of exit(0)
 print(
     f"Change report for {display_name} from {sorted_hsps[0]['captured_date']} to {sorted_hsps[-1]['captured_date']}\n\n"
 )
